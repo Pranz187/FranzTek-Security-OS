dfc99fabc7c57a0b31a91957d2d258b7bc44eb81
Pranz187 <joe187@live.com.au>
2026-06-30 19:31:48 +1000

    Fix config.js formatting issue

--- config/config.js
+++ config/config.js
@@ -6,5 +6,4 @@ module.exports = {
   haToken: process.env.HA_TOKEN,
   frigateUrl: process.env.FRIGATE_URL,
   weatherEntity: process.env.WEATHER_ENTITY || "weather.forecast_home",
-  alexaFrontDoorButton: process.env.ALEXA_FRONT_DOOR_BUTTON || "button.fkeller187_gmail_com_show_front_door"
-};
+  alexaFrontDoorButton: process.env.ALEXA_FRONT_DOOR_BUTTON || 
